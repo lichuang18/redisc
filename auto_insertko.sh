b78f256d135cd076b920f3908973a142329c2fbb
lichuang18 <983459843@qq.com>
2026-06-15 14:55:40 +0800

    new_motivation

--- auto_insertko.sh
+++ auto_insertko.sh
@@ -3,6 +3,7 @@ umount /mnt
 modprobe f2fs
 rmmod ef2fs.ko
 insmod ef2fs.ko
-mkfs.f2fs -f /dev/nvme2n1
-mount -t ef2fs  -o mode=lfs /dev/nvme2n1 /mnt
+mkfs.f2fs -f /dev/nvme2n1p1
+mount -t ef2fs  -o mode=lfs /dev/nvme2n1p1 /mnt
 # mount -t ef2fs  -o mode=lfs /dev/nvme2n1 /mnt
+dmesg -C
